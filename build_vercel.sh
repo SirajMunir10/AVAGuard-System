fe85fd7566a58c3a1a0655be738e9dd48457140f
SirajMunir10 <sirajmunir10@example.com>
2026-06-29 05:52:59 +0500

    Simplify Vercel deployment without build script

--- build_vercel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Install requirements using the flag to override the externally managed environment
-python3 -m pip install -r web_portal/requirements.txt --break-system-packages
-
-# Run migrations
-python3 web_portal/manage.py migrate --noinput
-
-# Collect static files
-python3 web_portal/manage.py collectstatic --noinput
